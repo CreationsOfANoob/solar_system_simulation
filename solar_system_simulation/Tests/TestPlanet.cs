4bd6b3c5272b96509c956f9f1cd59af7cbe8b08b
Isak <isak@waltin.com>
2022-07-02 20:40:46 +0200

    Re reverted

---
+++ solar_system_simulation/Tests/TestPlanet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class TestPlanet
+    {
+        [Theory]
+        [InlineData("Test Planet")]
+        [InlineData("Other Test Planet")]
+        public void Constructor_Sets_Name(string name)
+        {
+            var testPlanet = new solar_system_simulation.Planet(name);
+            Assert.Equal(name, testPlanet.Name);
+        }
+    }
+}
